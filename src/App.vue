c9ab54f943badc88b8171895f84b9a603afb1056
Imre Toth <tothimre@designtech.hu>
2020-05-24 09:46:57 +0200

    Added vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>arthanis.github.io</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+};
+</script>
+
+<style lang="scss">
+  @import 'assets/scss/app';
+</style>
