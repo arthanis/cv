2c051b477de3b580ca9d3d66f014eaeaffcb1c22
Imre Toth <tothimre@designtech.hu>
2021-01-30 08:49:57 +0100

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,24 +4,34 @@
 set -e
 
 # new orphan branch
-git checkout --orphan gh-pages
+# git checkout --orphan gh-pages
 
 # build
 npm run build
 
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:arthanis/cv.git master:gh-pages
+
 # new work tree
-git --work-tree dist add --all
+# git --work-tree dist add --all
 
 # commit
-git --work-tree dist commit -m 'Deploy'
+# git --work-tree dist commit -m 'Deploy'
 
 # push
-git push origin HEAD:gh-pages --force
+# git push origin HEAD:gh-pages --force
 
 # checkout master
-git checkout -f master
+# git checkout -f master
 
 # remove local gh-pages branch
-git branch -D gh-pages
+# git branch -D gh-pages
 
-cd ~
+cd -
