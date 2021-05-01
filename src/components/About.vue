52429bab2cbfec4bd61582a61b6aa25a8594b2be
Imre Toth <tothimre@designtech.hu>
2021-05-01 14:00:03 +0200

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -9,11 +9,10 @@
                class="section__image rounded-circle">
         </div>
         <div class="col-md-8 col-lg-9 pt-4 pt-md-0">
-          <p>Senior frontend developer with backend developing experience from Budapest.
-            <br>Currently I am working as a contractor for multiple companies.</p>
+          <p>Senior frontend developer with backend developing experience from Budapest.</p>
 
-          <p><strong>Used technologies:</strong> HTML5, CSS3 / SCSS, Bootstrap, JavaScript (ES6),
-            Vue.js, webpack, Erlang, gulp, npm / yarn, SVG, XML, jQuery, Git.</p>
+          <p><strong>Used technologies:</strong> HTML5, CSS3 (SCSS), JS (ES6+),
+            Vue.js, React, Bootstrap, webpack, gulp, npm / yarn, SVG, XML, jQuery, Git.</p>
 
           <p class="d-none">Looking for javascript developer opportunities and I am opened to work with
             node.js.</p>
