ba941ba5bfa6349bdb55eb392587b0eac5d62f1f
Imre Toth <tothimre@designtech.hu>
2021-05-01 14:02:52 +0200

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -11,8 +11,10 @@
         <div class="col-md-8 col-lg-9 pt-4 pt-md-0">
           <p>Senior frontend developer with backend developing experience from Budapest.</p>
 
-          <p><strong>Used technologies:</strong> HTML5, CSS3 (SCSS), JS (ES6+),
-            Vue.js, React, Bootstrap, webpack, gulp, npm / yarn, SVG, XML, jQuery, Git.</p>
+          <p>
+            <strong>Used technologies:</strong><br>
+            HTML5, CSS3 (SCSS), JS (ES6+), Vue.js, React, Bootstrap, webpack, gulp, npm / yarn, SVG, XML, Git.
+          </p>
 
           <p class="d-none">Looking for javascript developer opportunities and I am opened to work with
             node.js.</p>
